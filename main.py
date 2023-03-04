f8941593fc222890b5efc21dedc86855f75d2a83
Shengkai Chen <shengkaikenchen@gmail.com>
2023-03-04 06:29:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-    import pandas as pd
-    from sklearn.model_selection import train_test_split
-    from AnomalyDetection import AnomalyDetection
-    df = pd.read_csv("./data/creditcard.csv", sep=",", header=0)
-    AnomalyDetection.read_label(df, "Class")
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from models import AnomalyDetection
+from helper_functions import Tools
 
-    a1 = AnomalyDetection.isolation_forest(df, 0.2)
-    a2 = AnomalyDetection.elliptical_envelope(df, 0.2)
+df = pd.read_csv("./data2/creditcard.csv", sep=",", header=0)
+Tools.read_label(df, "Class")
+
+a1 = AnomalyDetection.isolation_forest(df, 0.2)
+a2 = AnomalyDetection.elliptical_envelope(df, 0.2)
